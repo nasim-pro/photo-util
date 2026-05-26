d59d323e5713b91d29291ce032f01f0d8908b78d
Nasim Ahmad <khannasimahmad@gmail.com>
2026-05-26 09:09:13 +0530

    fix(next.config.ts): Update images and base paths for production deployment

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,11 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: "export",
-
-  images: { unoptimized: true,},
-
-  trailingSlash: true,
-
-  basePath: isProd ? "/utils" : "",
-
-  assetPrefix: isProd ? "/utils/" : "",
+    output: "export",
+    images: { unoptimized: true },
+    trailingSlash: true,
+    basePath: isProd ? "/utils" : "",
+    assetPrefix: isProd ? "/utils/" : "", // Keep this as is
 };
 
 export default nextConfig;
